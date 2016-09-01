b74e4d4c78e2fa2bb4ca3f70ceaf70550f4a0663
Linda Zou <linda.ye.zou@gmail.com>
2016-09-01 11:47:11 -0700

    Sets up serverside logic for bug post request

--- route/bugRoute.ts
+++ route/bugRoute.ts
@@ -3,7 +3,19 @@ let router = express.Router();
 let mongoose = require('mongoose');
 let Bug = require('../model/bugModel')
 
-
-
-
+router.post('/bugRoute', function(req, res, next) {
+  let newBug = new Bug(
+    {
+    bugName: req.body.bugName,
+    description: req.body.description,
+    discoveredBy: req.body.discoveredBy,
+    assignedTo: req.body.assignedTo,
+    date_created: new Date()
+  }
+);
+  newBug.save(function(err, bug) {
+       if(err) return next(err);
+       res.send("This bug has been added");
+  });
+});
 export = router;
