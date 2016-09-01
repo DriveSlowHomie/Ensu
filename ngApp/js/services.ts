dfe443fdc69bb539925691c9cb741109ff73e68c
Linda Zou <linda.ye.zou@gmail.com>
2016-09-01 16:01:40 -0700

    Implements working post request for bugs

--- ngApp/js/services.ts
+++ ngApp/js/services.ts
@@ -6,11 +6,16 @@ namespace app.Services {
     constructor(
       private $resource: ng.resource.IResourceService
     ){
-      this.BugResource = $resource('/api/route/bugRoute/:id')
+      this.BugResource = $resource('/api/bugs/:id')
     }
 
     public query(){
       return this.BugResource.query()
+    }
+
+    public add(bug){
+      console.log(`this is being called in the services ${bug.bugName}`)
+      return this.BugResource.save(bug).$promise
     }
 
   }
